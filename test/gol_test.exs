2d84ccc92f0a8c1eff8310cda9feb93737df2b4d
Will Strinz <wstrinz@gmail.com>
2018-03-30 21:17:09 -0500

    another test

--- test/gol_test.exs
+++ test/gol_test.exs
@@ -12,7 +12,7 @@ defmodule GolTest do
     assert Gol.live_neighbors(@example_state, [1, 1]) == 3
   end
 
-  test "tick" do
+  test "square tick" do
     example_next_state = [
       [0, 0],
       [0, 1],
@@ -21,5 +21,23 @@ defmodule GolTest do
     ]
 
     assert Gol.tick(@example_state) == example_next_state
+  end
+
+  test "L tick" do
+    l_shape = [
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [2, 0]
+    ]
+
+    l_shape_next = [
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, -1]
+    ]
+
+    assert Gol.tick(l_shape) == l_shape_next
   end
 end
